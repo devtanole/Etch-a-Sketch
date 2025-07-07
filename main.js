4507d132b79e77e4228db0e15376f2c70f96fba1
Devin Olesen <devintanielu@gmail.com>
2025-07-07 01:13:15 +0000

    draw when mousedown

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
 const grid = document.querySelector(".grid-container");
+const colorPicker = document.querySelector(".color-picker");
+
+let isMouseDown = false;
+let currentColor = colorPicker.value;
+
+colorPicker.addEventListener("input", (e) => {
+  currentColor = e.target.value;
+});
 
 for (let i = 0; i < 16; i++) {
   for (let j = 0; j < 16; j++) {
@@ -8,6 +16,30 @@ for (let i = 0; i < 16; i++) {
   }
 }
 
-document.querySelector(".color-picker").onchange = (e) => {
-  console.log(e.target.value);
-};
+document.body.addEventListener("mousedown", () => {
+  isMouseDown = true;
+});
+document.body.addEventListener("mouseup", () => {
+  isMouseDown = false;
+});
+
+const buttons = document.querySelectorAll(".btn");
+buttons.forEach((button) => {
+  button.addEventListener("click", function () {
+    buttons.forEach((btn) => btn.classList.remove("active"));
+    this.classList.add("active");
+  });
+});
+
+const cells = document.querySelectorAll(".cell");
+cells.forEach((cell) => {
+  cell.addEventListener("mousedown", () => {
+    cell.style.backgroundColor = currentColor;
+  });
+
+  cell.addEventListener("mouseover", () => {
+    if (isMouseDown) {
+      cell.style.backgroundColor = currentColor;
+    }
+  });
+});
